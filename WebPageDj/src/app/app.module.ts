8e9482f3256c76811063a9248fb47edf738f1574
RaymanKing <enrikerapela@gmail.com>
2021-02-24 03:53:09 +0100

    Componente header y home

--- WebPageDj/src/app/app.module.ts
+++ WebPageDj/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PlaylistComponent } from './playlist/playlist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PlaylistComponent
   ],
   imports: [
     BrowserModule,
